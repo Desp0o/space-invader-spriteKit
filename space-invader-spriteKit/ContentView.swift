16ad1fdd9f43c29cf011704948b8d5fef373d6bf
desp0o <tornike.despotashvili@gmail.com>
2025-06-28 23:09:46 +0400

    proj init

---
+++ space-invader-spriteKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  space-invader-spriteKit
+//
+//  Created by Despo on 28.06.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Gamer!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ space-invader-spriteKit/space_invader_spriteKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  space_invader_spriteKitApp.swift
+//  space-invader-spriteKit
+//
+//  Created by Despo on 28.06.25.
+//
+
+import SwiftUI
+
+@main
+struct space_invader_spriteKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
