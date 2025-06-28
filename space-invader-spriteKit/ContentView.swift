a990f1e37b1cd8916a3c9b9905dc74b99faadc2f
desp0o <tornike.despotashvili@gmail.com>
2025-06-28 23:34:34 +0400

    add ship

--- space-invader-spriteKit/ContentView.swift
+++ space-invader-spriteKit/ContentView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 import SpriteKit
 
 struct ContentView: View {
-  let scene: SKScene = MainScene()
+  let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
+    
+    var scene: SKScene {
+      let scene = MainScene()
+      scene.size.width = width
+      scene.size.height = height
+      scene.scaleMode = .fill
+      
+      return scene
+    }
   
   var body: some View {
     SpriteView(scene: scene)
@@ -23,9 +33,13 @@ struct ContentView: View {
 
 
 final class MainScene: SKScene {
+  let spaceShip = SKSpriteNode(imageNamed: "spaceShip")
+  
   override func didMove(to view: SKView) {
-    self.scaleMode = .fill
+    self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+    
     setupBG()
+    setupSpaceShip()
   }
   
   func setupBG() {
@@ -35,5 +49,16 @@ final class MainScene: SKScene {
     background.zPosition = -1
     
     addChild(background)
+  }
+  
+  func setupSpaceShip() {
+    spaceShip.name = "spaceShip"
+    spaceShip.position = CGPoint(x: size.width / 2, y: 50)
+    spaceShip.size = CGSize(width: 70, height: 70)
+    spaceShip.physicsBody = SKPhysicsBody(rectangleOf: spaceShip.frame.size)
+    spaceShip.physicsBody?.isDynamic = true
+    spaceShip.physicsBody?.affectedByGravity = false
+    
+    addChild(spaceShip)
   }
 }
